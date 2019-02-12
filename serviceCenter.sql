3e6b30748271230234378a159b5263edf7552490
Hwi <hwice@hanmail.net>
2019-02-12 11:32:52 +0900

    sql update

--- serviceCenter.sql
+++ serviceCenter.sql
@@ -3,7 +3,10 @@
 create table serviceCenter (
 	title varchar2(30) not null,
 	titleNo number(10) primary key not null ,
+	place varchar2(30) not null,
 	uploadDate varchar2(30),
 	content varchar2(1000) not null
 	);
+	
+drop table serviceCenter purge;
 	
