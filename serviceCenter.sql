abf2a5e3e573aca46b4faad6d0663c3f2573f392
Hwi <hwice@hanmail.net>
2019-02-12 11:26:10 +0900

    123

---
+++ serviceCenter.sql
@@ -0,0 +1,9 @@
+
+	공지사항 테이블
+create table serviceCenter (
+	title varchar2(30) not null,
+	titleNo number(10) primary key not null ,
+	uploadDate varchar2(30),
+	content varchar2(1000) not null
+	);
+	
