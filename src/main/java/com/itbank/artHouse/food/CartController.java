82430eed71bac972e7f38dc028a80d01013689a5
LigerKing <qhsh1237@gmail.com>
2019-03-08 14:18:19 +0900

    update food

--- src/main/java/com/itbank/artHouse/food/CartController.java
+++ src/main/java/com/itbank/artHouse/food/CartController.java
@@ -13,49 +13,55 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.itbank.artHouse.users.UserDAO;
+import com.itbank.artHouse.users.UserDTO;
+
 @Controller
 public class CartController {
-	
-	@Autowired
-	CartDAO dao2;
-	int total = 0;
-	ArrayList<CartDTO> list;
-	List<CartDTO> list2;
-	
-	
-	@RequestMapping("cartInsert.do")
-	public String cartInsert(CartDTO cartDTO,Model model, HttpSession session, @RequestParam("name") String name) throws Exception{
-		String user_id = (String)session.getValue("id");
-		cartDTO.setUser_id(user_id);
-		System.out.println("user_id: "+cartDTO.getUser_id());
-		System.out.println("상품코드: "+cartDTO.getCode());
-		System.out.println("가격: "+cartDTO.getPrice());
-		System.out.println("수량: "+cartDTO.getAmount());
-		total = cartDTO.getAmount()*cartDTO.getPrice();
-		cartDTO.setPrice(total);
-		
-		dao2.insert(cartDTO);  //db에 insert
-		list = new ArrayList<CartDTO>(); 
-		
-		list.add(cartDTO);
-		model.addAttribute("list", list);
-		
-		System.out.println("Insert 완료!");
-		System.out.println("---------------------------");
-		return "food/cartInsert";
-	}
-	
-	@RequestMapping("/cartView")
-	public String selectCart(CartDTO cartDTO,HttpServletRequest request,Model model) throws Exception{
-		
-		String user_id = request.getParameter("user_id");
-		return "food/cartView";
-	}
-	
-	/*@RequestMapping("test02.do")
-	public void selectTest(CartDTO cartDTO) throws Exception {
-		CartDTO dto2 = new CartDTO();
-		dto2 = dao2.select(cartDTO);
-		System.out.println(dto2.getPrice());
-	}*/
+   
+   @Autowired
+   CartDAO dao2;
+   int total = 0;
+   ArrayList<CartDTO> list;
+   List<CartDTO> list2;
+   
+   
+   @RequestMapping("cartInsert.do")
+   public String cartInsert(CartDTO cartDTO,Model model, HttpSession session, @RequestParam("name") String name,UserDTO userDTO) throws Exception{
+      
+      userDTO = (UserDTO)session.getAttribute("user");
+      cartDTO.setUser_id(userDTO.getId());
+      System.out.println("세션에 저장된 id값 가져오기: " +cartDTO.getUser_id());
+      
+      System.out.println("user_id: "+cartDTO.getUser_id());
+      System.out.println("상품코드: "+cartDTO.getCode());
+      System.out.println("가격: "+cartDTO.getPrice());
+      System.out.println("수량: "+cartDTO.getAmount());
+      total = cartDTO.getAmount()*cartDTO.getPrice();
+      cartDTO.setPrice(total);
+      
+      dao2.insert(cartDTO);  //db에 insert
+      list = new ArrayList<CartDTO>(); 
+      
+      list.add(cartDTO);
+      model.addAttribute("list", list);
+      
+      System.out.println("Insert 완료!");
+      System.out.println("---------------------------");
+      return "food/cartInsert";
+   }
+   
+   @RequestMapping("/cartView")
+   public String selectCart(CartDTO cartDTO,HttpServletRequest request,Model model) throws Exception{
+      
+      String user_id = request.getParameter("user_id");
+      return "food/cartView";
+   }
+   
+   /*@RequestMapping("test02.do")
+   public void selectTest(CartDTO cartDTO) throws Exception {
+      CartDTO dto2 = new CartDTO();
+      dto2 = dao2.select(cartDTO);
+      System.out.println(dto2.getPrice());
+   }*/
 }
